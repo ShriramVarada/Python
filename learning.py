a06568f016e8c18a258d34d8f726c37abe695bc3
Shriram Varadarajan <shriram.varadarajan@gmail.com>
2018-08-21 10:36:16 -0500

    if-elif-else statements

--- learning.py
+++ learning.py
@@ -23,5 +23,35 @@ print(tuple2)
 tuple2=('dfs','sf')
 print(tuple2)
 
+if tuple2 != (2,3,3):
+    print('They are not equal')
+
 tuple2=3
 print(tuple2)
+
+for x in mahajanas:
+    if x=='bali':
+        print(x + " is the grandson of prahlada")
+    elif x == 'narada':
+        print('Devershi Narada')
+    else:
+        print(x.title())
+
+x = 108
+if x > 109 or x< 210: # and, not as well
+    print('x <210 or x>109')
+
+nondevotee = 'demons'
+
+if nondevotee not in mahajanas:
+    print(nondevotee.title()+"is not a devotee")
+
+print(str(tuple2)+ " is a prime number")
+
+requested_toppings = []
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        print("Adding " + requested_topping + ".")
+        print("\nFinished making your pizza!")
+else:
+    print("Are you sure you want a plain pizza?")
