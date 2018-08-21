d77e7929521a3b396899f6e30471c2583521e944
Shriram Varadarajan <shriram.varadarajan@gmail.com>
2018-08-20 19:13:36 -0500

    tested lists

--- Sample.py
+++ Sample.py
@@ -1,1 +1,32 @@
 print("Hello")
+
+namesofGod = ['Narayana', 'Krishna', 'Rama' , 'Govinda']
+
+namesofGod.append('Narasimha')
+print(namesofGod)
+
+namesofGod.insert(2, 'Ajitah')
+print(namesofGod)
+
+del namesofGod[2]
+print(namesofGod)
+
+narayana = namesofGod.pop(0);
+print(namesofGod)
+
+govinda = 'Govinda'
+namesofGod.remove(govinda)
+print(namesofGod)
+
+print(govinda.title())
+
+namesofGod.sort()
+print(namesofGod)
+
+namesofGod.sort(reverse=True)
+print(namesofGod)
+
+print(sorted(namesofGod))
+namesofGod.sort()
+namesofGod.reverse()
+print(namesofGod[-1])
