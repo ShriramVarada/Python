ab7ec54895afb7a4321362be4845e480296f050c
Shriram Varadarajan <shriram.varadarajan@gmail.com>
2018-08-22 22:20:29 -0500

    Modules and classes

--- Dog.py
+++ Dog.py
@@ -1,14 +1,40 @@
-class Dog():
-	def __init__(self, name, age):
-		self.name = name
-		self.age = age
-	
-	def sit(self):
-		print(self.name.title() + " is now sitting")
-	
-	def roll_over(self):
-		print(self.name.title() + " is now rolling over")
+"""Class Dog Description"""
 
 
-my_dog = Dog('willie' , 6)
-print("My dog name is "+ my_dog.name.title() + ".")
+class Dog:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        print(self.name.title() + " is now sitting")
+
+    def roll_over(self):
+        print(self.name.title() + " is now rolling over")
+
+
+class Car:
+    def __init__(self, make, model, year=2009):
+        """Initialize attributes to describe a car."""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer = 0
+
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+        return long_name.title()
+
+    def odometer_reading(self):
+        print('The odometer is ' + str(self.odometer))
+
+    def set_odometer(self, number):
+        self.odometer=number
+
+    def inc_odometer(self, number):
+        if number > 0:
+            self.odometer += number
+        else:
+            print('Enter a positive value')
---
+++ pizza/electriccar.py
@@ -0,0 +1,48 @@
+from Dog import Car, Dog
+
+
+class Battery:
+    def __init__(self, battery_size=56):
+        self.battery_size = battery_size
+
+    def battery_lifepercent(self):
+        return self.battery_size*2/100
+
+    def range(self):
+        global range2
+        if self.battery_size > 80:
+            range2 = 30
+        elif self.battery_size < 80:
+            range2 = 20
+
+        message=''
+        message += "This car goes approx 90 miles /h"
+        print(message)
+        return range2
+
+
+class ElectricCar(Car):
+    def __init__(self, make, model, year=3):
+        """Need a default parameter in the subclass constructor"""
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print("This car has a " + str(self.battery.battery_size) + "-kWh battery. And life "
+                                                                   "percent is " +
+              str(self.battery.battery_lifepercent()) + " and the range is " + str(self.battery.range()))
+
+    def inc_odometer(self, number):
+        self.odometer += number*2
+
+
+car = ElectricCar('da', 'das', 43)
+car.inc_odometer(2)
+car.odometer_reading()
+car.describe_battery()
+
+d = Dog('ads', 45)
+print(d.age)
+ds = Car('as', 'Bn')
+print(ds.get_descriptive_name())
