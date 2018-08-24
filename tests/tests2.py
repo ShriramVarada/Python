abd38367cea1561fe3742769895dfc7ebdb09362
Shriram Varadarajan <shriram.varadarajan@gmail.com>
2018-08-23 20:26:30 -0500

    tests part 2

--- tests/tests2.py
+++ tests/tests2.py
@@ -3,17 +3,21 @@ from names import Survey
 
 
 class TestCases(unittest.TestCase):
+
+    def setUp(self):
+        self.survey = Survey('What is your name?')
+        self.responses = ['Narayana', 'Govinda', 'Krishna']
+
     def test_single(self):
-        survey = Survey('What is your name')
-        survey.store_response('Shriram')
-        self.assertIn('Shriram', survey.responses)
+
+        self.survey.store_response(self.responses[0])
+        self.assertIn(self.responses[0], self.survey.responses)
 
     def test_thrice(self):
-        survey = Survey('What is your names')
-        responses = ['sa', 'sdsd', 'ads']
-        for x in responses:
-            survey.store_response(x)
-        for x in responses:
-            self.assertIn(x, survey.responses)
+        for x in self.responses:
+            self.survey.store_response(x)
+        for x in self.responses:
+            self.assertIn(x, self.survey.responses)
+
 
 unittest.main()
